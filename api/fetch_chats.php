42ea07c0672fa9c5ac29de0cb80ef36c4b0033c7
pya-h <p4ya.py@gmail.com>
2026-01-13 16:56:42 +0330

    feat: list friends by their last interaction date

--- api/fetch_chats.php
+++ api/fetch_chats.php
@@ -16,11 +16,11 @@ $stmt = $pdo->prepare('
   SELECT DISTINCT u.username
   FROM users u
   JOIN (
-    SELECT sender_id as user_id FROM messages WHERE receiver_id = ?
+    SELECT sender_id as user_id, created_at as interaction_date FROM messages WHERE receiver_id = ?
     UNION
-    SELECT receiver_id as user_id FROM messages WHERE sender_id = ?
+    SELECT receiver_id as user_id, created_at as interaction_date FROM messages WHERE sender_id = ?
   ) m ON u.id = m.user_id
-  WHERE u.id != ?
+  WHERE u.id != ? ORDER BY interaction_date DESC
 ');
 $stmt->execute([$userId, $userId, $userId]);
 $users = $stmt->fetchAll(PDO::FETCH_COLUMN);
